64a3d6fcd68800c8db3be9aa852dca93090e675b
Saranya-sharvi <a.saranya21214@gmail.com>
2018-10-02 16:41:27 +0530

    new trining programs

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-var1 ="5"
-var2 ="10"
+var1 = 5
+var2 = 10 
+var3 = 6 
 
-
-
-if("var1">="var2"):
+if((var1 >= var2 )and (var1 >= var3)):
 	
-	print("var1")
+	print("The biggest is :", var1)
+
+if(var2 >= var3):
+	print("The biggest is :", var2)
+
 else:
-	print("var2")
-
-
+	print("The biggest is :", var3)
