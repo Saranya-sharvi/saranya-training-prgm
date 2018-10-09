c2149e0b23f1c8ecbf809ca8efe2e9d7a60b4579
Saranya-sharvi <a.saranya21214@gmail.com>
2018-10-09 09:49:32 +0530

    answer.txt

---
+++ fuct.py
@@ -0,0 +1,25 @@
+
+def cse(staff,sub):
+    print "staff: ",staff
+    print "sub: ",sub
+    return;
+dict={}
+        
+cse(staff="surya",sub="AI")
+   
+def ece(staff,sub):
+    print "staff: ",staff
+    print "sub: ",sub
+    return;
+        
+cse(staff="vijay",sub="adc")
+
+def eee(staff,sub):
+    print "staff: ",staff
+    print "sub: ",sub
+    return;
+        
+
+cse(staff="ajith",sub="ec")
+
+
