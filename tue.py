f2f18f15d374ff7cf9cfd6e011903f49b9bacb4d
Saranya-sharvi <a.saranya21214@gmail.com>
2018-10-09 16:36:25 +0530

    tue.py

--- tue.py
+++ tue.py
@@ -1,17 +1,27 @@
+""" assign of class"""
 class maths():
+    """ perform division operation """
+
     def division(a,b):
         c=a%b
         return c
+    """ perform addition operation """
+
     def addition(a,b):
         c=a+b
         return c
+    """ perform subtraction operation """
+
     def subtaction(a,b):
         c=a-b
         return c
+    """get the input from user"""
+
 
 a=int(input("enter the value of a:"))
 b=int(input("enter the value of b:"))
 
+    
 ans={}
 ans["division"]=maths.division(a,b)
 ans["addition"]=maths.addition(a,b)
